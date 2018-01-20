5061ccb6ba965b658ad0a6e52a616d95f4159455
Kevin O'Neal <oneal.kevin@gmail.com>
2018-01-19 23:03:14 -0600

    add serv tool

--- bin/serv.sh
+++ bin/serv.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-die() { echo "script failure $0: $@" >&2; exit 111; }
+die() { echo "Script failure $0: $@" >&2; exit 111; }
 
-declare -A servers
+CFG=~/servers.cfg
 
-servers=(
-    ["server1"]="server1.tmp"
-          ["server1"]="server2.tmp"
-        )
+if [ ! -f  $CFG ]; then
+    die must have $CFG
+else
+    source $CFG
+fi
+
+if [ -z "${servers[$11]}" ] || [ -z "${servers[$12]}" ]; then
+    die
+fi
 
 echo Logging into: ${servers[$11]} and  ${servers[$12]}
 
-if [ -z "${servers[$12]}" ]; then
-    echo it is
-fi
-
-
-tmux new-window -n $1
-tmux send-keys "ssh koneal@${servers[$11]}.dfwx.masergy.com" C-m
-tmux split-window -v
-tmux send-keys "ssh koneal@${servers[$12]}.dfwx.masergy.com" C-m
+#tmux new-window -n $1
+#tmux send-keys "ssh koneal@${servers[$11]}.$domain" C-m
+#tmux split-window -v
+#tmux send-keys "ssh koneal@${servers[$12]}.$domain" C-m
