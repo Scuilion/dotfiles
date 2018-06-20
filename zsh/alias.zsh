e4c9670e203a73a5f3302d0d6798f1233297d35b
Kevin ONeal <oneal.kevin@gmail.com>
2018-06-20 16:07:28 -0500

    Stop using oh-my-zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,4 +1,21 @@
+#DIRECTORIES
 
+## Changing/making/removing directory
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+alias md='mkdir -p'
+alias rd=rmdir
+alias d='dirs -v | head -10'
+
+## List directory contents
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+
+#GIT
 alias gcf='git config --list'
 
 alias gcam='git commit -a -m'
