554d8c3ab94ad538458a146e66f5631483eed476
Kevin ONeal <oneal.kevin@gmail.com>
2018-06-20 20:07:23 -0500

    fix prompt theme

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,14 +1,7 @@
-#PROMPT='
-#%F{blue}%/%F{reset_color} $(git_super_status)%F{white}[%n@%m]%F{reset_color} %F{white}[%T]%F{reset_color}
+PROMPT='
+%F{blue}%/%F{reset_color} $(git_prompt_string)%F{white}[%n@%m]%F{reset_color} %F{white}[%T]
+ '
 #%F{black}>%F{reset_color} '
 
-#PROMPT='%B%m%~%b$(git_super_status) %# '
 
-#PROMPT2="%{$fg_blod[black]%}%_> %{$reset_color%}"
-
-#GIT_CB="git::"
-#ZSH_THEME_SCM_PROMPT_PREFIX="%{$fg[green]%}["
-#ZSH_THEME_GIT_PROMPT_PREFIX=$ZSH_THEME_SCM_PROMPT_PREFIX$GIT_CB
-#ZSH_THEME_GIT_PROMPT_SUFFIX="]%{$reset_color%} "
-#ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[red]%}*%{$fg[green]%}"
-#ZSH_THEME_GIT_PROMPT_CLEAN=""
+#%F{blue}%/%F{reset_color} $(git_prompt_string)%F{white}[%n@%m]%F{reset_color} %F{white}[%T]%F{reset_color}
