bb0c26f33cfece86c4d38d33f7292c231ddd98e9
Kevin O'Neal <kevin.oneal@masergy.com>
2021-08-16 10:50:02 -0500

    remove bell noise

--- gsettings.sh
+++ gsettings.sh
@@ -1,2 +1,3 @@
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ close-tab disabled
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ close-window disabled
+gsettings set org.gnome.desktop.sound event-sounds false
