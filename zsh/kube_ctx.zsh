7030d996dfb34f5e8cd1829203f62d40a0ca99e2
Kevin O'Neal <oneal.kevin@gmail.com>
2022-05-23 15:27:56 -0500

    do not throw errors when kubectl is not present

--- zsh/kube_ctx.zsh
+++ zsh/kube_ctx.zsh
@@ -1,9 +1,12 @@
 kube_ctx(){
-    cur_ctx=$(kubectl config current-context 2> /dev/null)
-    if [ $? -gt 0 ]; then
-        echo ""
-    else
-        ns=$(kubectl config view -o=jsonpath="{.contexts[?(@.name==\"${cur_ctx}\")].context.namespace}")
-        echo " %{$fg[cyan]%}$cur_ctx/$ns%{$reset_color%} "
+    if [ $commands[kubectl] ]; then
+        source <(kubectl completion zsh)
+        cur_ctx=$(kubectl config current-context 2> /dev/null)
+        if [ $? -gt 0 ]; then
+            echo ""
+        else
+            ns=$(kubectl config view -o=jsonpath="{.contexts[?(@.name==\"${cur_ctx}\")].context.namespace}")
+            echo " %{$fg[cyan]%}$cur_ctx/$ns%{$reset_color%} "
+        fi
     fi
 }
