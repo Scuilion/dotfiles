bfcff8374971fa9b98fcad96bc02ccdb0e2841b7
Kevin O'Neal <oneal.kevin@gmail.com>
2023-06-01 17:39:47 -0500

    disable lock screen notifications so that the displays will stay shutdown

--- gsettings.sh
+++ gsettings.sh
@@ -1,5 +1,8 @@
+# to figure out what settings are being changed via the UI, run the following
+# `G_MESSAGES_DEBUG=all gnome-control-center`
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ close-tab disabled
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ close-window disabled
 gsettings set org.gnome.desktop.sound event-sounds false
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 28
+gsettings set org.gnome.desktop.notifications show-in-lock-screen false
